1b4bc8d525e42619294ff87d1907965f908511e9
DiTulio <viniciusditulio@gmail.com>
2026-09-01 19:46:43 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,26 @@
 import './App.css'
 import hero from './assets/hero.png'
+import Benefits from './components/Benefits'
 import Categories from './components/Categories'
 import Footer from './components/Footer'
 import Header from './components/Header'
-import Hero from './components/Hero'
-import Products from './components/Produtos'
+import HeroSection from './components/HeroSection'
+import Products from './components/Products'
 
 const App = () => {
   return (
     <div className="app">
-      
-      <Header/>
-      
-      <Products/>
-      
-      <Categories/>
-      
-      <Footer/>
-      
-      <Hero/>
-      
-      <Benefits/>
-      
-
+      <Header />
+      <HeroSection 
+        titulo ="Ofertas imperdíveis para você"
+        subtitulo = "Até 30% de desconto em produtos selecionados. Aproveite!"
+        textoBotao = "Ver ofertas"
+        imagemHero = {hero}
+      />
+      <Categories />
+      <Products />
+      <Benefits />
+      <Footer />
     </div>
   )
 }
