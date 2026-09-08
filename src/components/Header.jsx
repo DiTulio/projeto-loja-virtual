39e267bac40c4de37767a897ee252948bb8d60f2
DiTulio <viniciusditulio@gmail.com>
2026-09-08 19:46:36 -0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,13 +4,13 @@ const Header = () => {
     return (
         <header className="header">
             <div className="logo">🛍️ ReactShop</div>
-
+        
             <ul className="nav">
-                <li><link to="/"></link></li>
-                <li><link to="/produtos">Produtos</link></li>
-                <li><link to="/contato">Contato</link></li>
+                <li><Link to="/">Início</Link></li>
+                <li><Link to="produtos">Produtos</Link></li>
+                <li><Link to="contato">Contato</Link></li>
             </ul>
-
+        
             <div className="header-actions">
                 <span className="icon-button">🔍</span>
                 <span className="icon-button">❤️</span>
@@ -20,7 +20,7 @@ const Header = () => {
                 </span>
             </div>
         </header>
-    )
+  )
 }
 
 export default Header
